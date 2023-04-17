3ab0e563b76ec9fef0f369e77b9db2228c46aab5
lengushuru charles <charleslengushuru@gmail.com>
2023-04-17 17:02:55 +0300

    Add styling to header.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,15 @@ import logo from '../assets/planet.png';
 export default function Navbar() {
   return (
     <>
-      <header>
-        <a className="logo" href="/">
+      <header className="navbar">
+        <a className="navbar-img" href="/">
           <img src={logo} alt="Company Logo" />
           <h1>Space Travelers&apos; Hub</h1>
         </a>
-        <nav className="navbar">
+        <nav className="navbar-links">
           <NavLink to="/">Rockets</NavLink>
           <NavLink to="missions">Missions</NavLink>
-          <hr />
+          <hr className="line" />
           <NavLink to="myProfile">My Profile</NavLink>
         </nav>
       </header>
