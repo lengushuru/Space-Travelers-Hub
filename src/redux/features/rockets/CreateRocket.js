30202ee89a9e419946a0baacb2f21902d3ca27b1
Abdus Sattar <sasorganicagro@gmail.com>
2023-04-18 19:26:36 +0600

    Add handleImageError metho

--- src/redux/features/rockets/CreateRocket.js
+++ src/redux/features/rockets/CreateRocket.js
@@ -1,13 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux';
 import styles from './Rockets.module.css';
 import { toggleReservedState } from './rocketSlice';
+import falcon1 from '../../../assets/falcon1.jpg';
 
 const Rocket = () => {
   const rockets = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
+  const handleImageError = (event) => {
+    event.target.src = falcon1;
+  };
 
   if (rockets.isLoading) {
-    return <h1>Loading...</h1>;
+    return <h1> Please wait, Rockets are Loading...</h1>;
   }
 
   if (rockets.error) {
@@ -18,11 +22,11 @@ const Rocket = () => {
     <ul className={styles.rocketUL}>
       {rockets.rockets.map((rocket) => (
         <li key={rocket.id}>
-          <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
+          <img src={rocket.flickr_images[0]} onError={handleImageError} alt={rocket.rocket_name} />
           <div>
-            <p>{rocket.rocket_name}</p>
+            <p className={styles.name}>{rocket.rocket_name}</p>
             <p>
-              {rocket.reserved && <span className="reserved-badge">reserved</span>}
+              {rocket.reserved && <span className={styles.reservedBadge}>reserved</span>}
               {rocket.description}
             </p>
             <button
