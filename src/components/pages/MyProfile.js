5e36af6bff7d4f7cf6d20b41e5509240334a89ce
Abdus Sattar <sasorganicagro@gmail.com>
2023-04-19 04:28:21 +0600

    Final

--- src/components/pages/MyProfile.js
+++ src/components/pages/MyProfile.js
@@ -5,15 +5,31 @@ import styles from '../../redux/features/rockets/MyProfile.module.css';
 const MyProfile = () => {
   const rockets = useSelector((state) => state.rockets.rockets);
   const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+  const missions = useSelector((state) => state.mission001.missions);
+  const joinedMission = missions.filter((mission) => mission.reserved);
 
   return (
     <div className={styles.mainContainer}>
       <div className={styles.missionContainer}>
-        <p>missions container</p>
+        <h2>My Missions</h2>
+        <ul className={styles.UL}>
+          {joinedMission.length ? (
+            joinedMission.map((mission) => (
+              <li key={mission.id}>{mission.mission_name}</li>
+            ))
+          ) : (
+            <>
+              <li>
+                It looks like you haven&apos;t Joined any Missions yet.
+                Don&apos;t forget to check out the Missions page to make a reservation.
+              </li>
+            </>
+          )}
+        </ul>
       </div>
       <div className={styles.rocketContainer}>
         <h2>My Rockets</h2>
-        <ul className={styles.rocketsUL}>
+        <ul className={styles.UL}>
           {reservedRockets.length ? (
             reservedRockets.map((rocket) => (
               <li key={rocket.id}>{rocket.name}</li>
