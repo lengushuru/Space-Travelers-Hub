c15456127c3932f906ac729b878f0ea4eb0e6d6c
lengushuru charles <charleslengushuru@gmail.com>
2023-04-19 13:16:36 +0300

    display missions in Myprofile page.

--- src/components/pages/MyProfile.js
+++ src/components/pages/MyProfile.js
@@ -4,12 +4,28 @@ import styles from '../../redux/features/rockets/MyProfile.module.css';
 
 const MyProfile = () => {
   const rockets = useSelector((state) => state.rockets.rockets);
+  const missions = useSelector((state) => state.mission001.missions);
   const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+  const reservedMissions = missions.filter((mission) => mission.reserved);
 
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.missionContainer}>
-        <p>missions container</p>
+      <div className={styles.rocketContainer}>
+        <h2>My Missions</h2>
+        <ul className={styles.rocketsUL}>
+          {reservedMissions.length ? (
+            reservedMissions.map((rocket) => (
+              <li key={rocket.mission_id}>{rocket.mission_name}</li>
+            ))
+          ) : (
+            <>
+              <li>
+                It looks like you haven&apos;t joined any mission yet.
+                please navigate to Missions section and select missions to join
+              </li>
+            </>
+          )}
+        </ul>
       </div>
       <div className={styles.rocketContainer}>
         <h2>My Rockets</h2>
